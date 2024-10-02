25e86a7b3a8edd070467b01786f2ef753f4c20cf
Kostia S <kosiak5587@gmail.com>
2024-10-02 16:23:08 +0600

    feat: add solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+  const content = 'I am fresh and young';
+
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      try {
+        await fs.writeFile(filePath, content);
+        console.log('Файл fresh.txt успешно создан в папке files!');
+      } catch (writeError) {
+        console.error(writeError);
+      }
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await create();
