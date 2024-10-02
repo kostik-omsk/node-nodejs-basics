cd2105344e61e00d1e7aed38d6a9f1e576c7abf5
Kostia S <kosiak5587@gmail.com>
2024-10-02 22:03:57 +0600

    feat: add solution delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+    await fs.unlink(filePath);
+    console.log('File removed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('FS operation failed');
+    }
+  }
 };
 
 await remove();
