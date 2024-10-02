9c3e4ed8aa0b1cc14913652f9b636065007d2945
Kostia S <kosiak5587@gmail.com>
2024-10-02 23:03:27 +0600

    feat: add solution args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argsValues = args.reduce((acc, arg, i) => {
+    if (arg.startsWith('--') && args[i + 1]) {
+      return [...acc, `${arg} is ${args[i + 1]}`];
+    }else{
+      return acc;
+    }
+  }, []);
+
+  console.log(argsValues.join(', '));
 };
 
 parseArgs();
