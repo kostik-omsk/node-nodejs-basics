c62d728ab0bab8cec6bf869895df34c0c2f4fd80
Kostia S <kosiak5587@gmail.com>
2024-10-02 22:14:22 +0600

    feat: add solution list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(path.join(__dirname, 'files'));
+    files.forEach((file) => console.log(file));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('FS operation failed');
+    }
+  }
 };
 
 await list();
