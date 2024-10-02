24351d94dcb3ab22bff8b32ae0e7897bf5cbecc7
Kostia S <kosiak5587@gmail.com>
2024-10-02 22:17:10 +0600

    feat: add solution read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const content = await fs.readFile(filePath, 'utf8');
+    console.log(content);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('FS operation failed');
+    }
+  }
 };
 
 await read();
