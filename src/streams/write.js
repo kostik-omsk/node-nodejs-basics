26d27dc0d7fe022d7ca1b712635bcc3c0a6d8bf2
Kostia S <kosiak5587@gmail.com>
2024-10-03 18:07:39 +0600

    feat: add solution write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'node:stream/promises';
+import { getDirname } from '../helpers/index.js';
+
+const __dirname = getDirname(import.meta.url);
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const outputStream = createWriteStream(filePath, { encoding: 'utf8' });
+
+  try {
+    await pipeline(process.stdin, outputStream);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await write();
