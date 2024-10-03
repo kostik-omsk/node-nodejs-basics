6c1551a48d2798251de3ae137b10938030bedc22
Kostia S <kosiak5587@gmail.com>
2024-10-03 17:24:42 +0600

    feat: add solution calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs';
+import { pipeline } from 'node:stream/promises';
+import { getDirname } from '../helpers/index.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = crypto.createHash('sha256');
+
+  try {
+    const readStream = fs.createReadStream(filePath);
+    await pipeline(readStream, hash);
+    console.log(hash.digest('hex'));
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 await calculateHash();
