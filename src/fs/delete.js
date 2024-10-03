695c96561cd3ca193562173e866af9dafb71c1aa
Kostia S <kosiak5587@gmail.com>
2024-10-03 16:09:17 +0600

    refractor: use helper in delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,19 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { getDirname, FSError } from '../helpers/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = getDirname(import.meta.url);
 
 const remove = async () => {
   try {
     const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
     await fs.unlink(filePath);
-    console.log('File removed');
   } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.log('FS operation failed');
-    }
+    throw new FSError();
   }
 };
 
