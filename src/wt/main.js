a5d1482b9084109edb2e9de2ca841a6afb684a35
Kostia S <kosiak5587@gmail.com>
2024-10-04 13:51:24 +0600

    feat: add solution Worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const workerFile = new URL('./worker.js', import.meta.url);
+const cpu = cpus();
+// const cpu = Array.from({ length: 4 }); // you can set the number of default threads
+
 const performCalculations = async () => {
-    // Write your code here
+  let number = 10;
+
+  const arrWorkers = await Promise.allSettled(
+    cpu.map(
+      (_, i) =>
+        new Promise((resolve, reject) => {
+          const worker = new Worker(workerFile, { workerData: number + i });
+          worker.on('message', resolve);
+          worker.on('error', reject);
+        }),
+    ),
+  );
+
+  const arrResults = arrWorkers.map(({ status, value }) => {
+    return {
+      status: status === 'fulfilled' ? 'resolved' : 'error',
+      data: status === 'fulfilled' ? value : null,
+    };
+  });
+
+  console.log(arrResults);
 };
 
 await performCalculations();
