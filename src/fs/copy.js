18790ca400af7222a6e35e107681a91607a084d3
Kostia S <kosiak5587@gmail.com>
2024-10-04 09:42:15 +0600

    refactor: use fsPromises.cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,42 +5,18 @@ import { FSError } from '../helpers/errors.js';
 import { getDirname } from '../helpers/getDirname.js';
 
 const __dirname = getDirname(import.meta.url);
+
 const source = path.join(__dirname, 'files');
 const destination = path.join(__dirname, 'files_copy');
-
-const copyDirectory = async (src, dest) => {
-  try {
-    await fs.mkdir(dest, { recursive: true });
-
-    const entries = await fs.readdir(src, { withFileTypes: true });
-
-    const promises = entries.map(async (entry) => {
-      const sourcePath = path.join(src, entry.name);
-      const destinationPath = path.join(dest, entry.name);
-
-      if (entry.isDirectory()) {
-        await copyDirectory(sourcePath, destinationPath);
-      } else {
-        await fs.copyFile(sourcePath, destinationPath);
-      }
-    });
-
-    await Promise.all(promises);
-  } catch (error) {
-    throw new FSError(error.message);
-  }
-};
 
 const copy = async () => {
   try {
     const isSorceExists = await isExists(source);
     const isDestinationExists = await isExists(destination);
-
     if (!isSorceExists || isDestinationExists) {
       throw new Error('FS operation failed');
     }
-
-    await copyDirectory(source, destination);
+    await fs.cp(source, destination, { recursive: true });
   } catch (error) {
     throw new FSError(error.message);
   }
