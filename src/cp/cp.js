78bbd4539b5b18a843c6702c8c5618c3c93ef00c
Kostia S <kosiak5587@gmail.com>
2024-10-05 12:02:35 +0600

    feat: add solution cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { spawn } from 'child_process';
+import { getDirname } from '../helpers/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child_process_path = `${__dirname}/files/script.js`;
+
+  const child = spawn('node', [child_process_path, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
